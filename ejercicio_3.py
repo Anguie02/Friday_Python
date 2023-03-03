13d35a59570fea201667cd07425ed5b07406c06b
Anguie02 <1487186@senati.pe>
2023-03-03 11:42:22 -0500

    Convertimos una lista en tupla

--- ejercicio_3.py
+++ ejercicio_3.py
@@ -1,7 +1,21 @@
 #desarrollar un algoritmmo donde permita el ingrso de datos
-elemento = int(input("Ingrese el elemento: "))
+elemento = (input("Ingrese el elemento: "))
 #creamos un alista  
 lista = []
 #Añadimos el elemento4
 lista.append(elemento)
 print(lista)
+#converlimos la lista en una tupla
+tupla = tuple(lista)
+print(tupla)
+
+#probando otras formas
+#ejemplo = list((10, 15,27,100,200))
+#print(ejemplo)
+#for a in elemento:
+ #   lista.append(elemento)
+ #   print(lista)
+
+    
+
+
