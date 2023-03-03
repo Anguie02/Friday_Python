1bc738407f29745d2aabb2eccb454542df61eb6a
Anguie02 <1487186@senati.pe>
2023-03-03 09:37:34 -0500

    Realizamos el proceso y mostramos

--- Area_circulo.py
+++ Area_circulo.py
@@ -1,3 +1,9 @@
 #Area de un circulo
 #definimos una variable y pedimos al usuario que ingrese el valor del radio
 radio = float(input("Ingrese el  valor del radio: "))
+#importamos la libreria math
+from math import pi
+#Proceso para hallar el area del circulo
+area_circulo = pi *radio**2
+#mostramos el area
+print(area_circulo)
