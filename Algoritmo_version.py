2d9b583b574e052ce90a90eac485847c49b679ce
Anguie02 <1487186@senati.pe>
2023-03-03 08:59:04 -0500

    Otra forma de mostrar la versión de python

--- Algoritmo_version.py
+++ Algoritmo_version.py
@@ -1,4 +1,10 @@
 #desarrolle un algoritmo que muestre la version de python
 #Importamos el módulo 
-import sys
-print(sys.version)#mostramos la version
+#import sys
+#print(sys.version)#mostramos la version
+#mostramos más informacion
+#print(sys.version_info)
+#otra forma de mostrar la  version
+#iportamos 
+from platform import python_version
+print(python_version())
