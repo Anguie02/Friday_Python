ae43e422c6b8575cff867e03b5e31c60316a865d
Anguie02 <1487186@senati.pe>
2023-03-03 11:21:51 -0500

    Creamos una lista vacia y añadimos los elementos

---
+++ ejercicio_3.py
@@ -0,0 +1,7 @@
+#desarrollar un algoritmmo donde permita el ingrso de datos
+elemento = int(input("Ingrese el elemento: "))
+#creamos un alista  
+lista = []
+#Añadimos el elemento4
+lista.append(elemento)
+print(lista)
