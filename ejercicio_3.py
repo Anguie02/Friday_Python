3fe89f4cd2ffcdccb13baa90a85abcf9c2f224b2
Anguie02 <1487186@senati.pe>
2023-03-03 22:33:48 -0500

    Generamos el ingreso de valores y aplicamos el método split

--- ejercicio_3.py
+++ ejercicio_3.py
@@ -1,20 +1,17 @@
-#desarrollar un algoritmmo donde permita el ingrso de datos
-elemento = (input("Ingrese el elemento: "))
-#creamos un alista  
-lista = []
+#desarrollar un algoritmmo donde permita el ingreso de datos
+"""elemento = (input("Ingrese el elemento: "))"""
+#creamos una lista  
+"""lista = []"""
 #Añadimos el elemento4
-lista.append(elemento)
-print(lista)
-#converlimos la lista en una tupla
-tupla = tuple(lista)
-print(tupla)
-
-#probando otras formas
-#ejemplo = list((10, 15,27,100,200))
-#print(ejemplo)
-#for a in elemento:
- #   lista.append(elemento)
- #   print(lista)
+"""lista.append(elemento)"""
+"""print(lista)"""
+#convertimos la lista en una tupla
+"""tupla = tuple(lista)
+print(tupla)"""
+#Usamos el metodo para cadenas split()
+valores = input("Ingresa los valores separados: ")
+lista_valores = valores.split()
+print(lista_valores)
 
     
 
