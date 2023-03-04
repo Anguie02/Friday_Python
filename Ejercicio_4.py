4c474fd5c832ff6e9e73033e2e68bd7c845b2940
Anguie02 <1487186@senai.pe>
2023-03-04 09:28:14 -0500

    Indexamos el primer y último elemento y los imprimimos

--- Ejercicio_4.py
+++ Ejercicio_4.py
@@ -1,2 +1,6 @@
 #Progrma que permita imprimir el primer y ultimo  valor de la lista
-lista = [34, 56, 4]
+#Pedimos el ingreso de los elementos para la lista
+datos =input("Ingrese los datos separados:")
+lista = datos.split()
+#Imprimimos el primer y ultimo elemento
+print("El primer elemento es :", lista[0], "y el úlimo es: ", lista[len(lista)-1])
